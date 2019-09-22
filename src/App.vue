00948aca1491b56b26405a1a06dd9652aa5e1dc0
lsq210 <1135595767@qq.com>
2019-09-22 13:26:15 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      轨迹库可视化系统
+    </div>
+    <div class="aside">
+      欢迎使用！
+      <el-select v-model="selectedUser" multiple placeholder="请选择用户 ID">
+        <el-option
+          v-for="item in options"
+          :key="item.userId"
+          :label="item.label"
+          :value="item.data">
+        </el-option>
+      </el-select>
+      <el-button round v-on:click="print">显示</el-button>
+    </div>
+    <div class="main">
+      <baidu-map class="map" :center="{lng: 116.404, lat: 39.915}" :zoom="15">
+        <bm-marker :position="{lng: 116.404, lat: 39.915}">
+        </bm-marker>
+      </baidu-map>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      selectedUser: [],
+      options: [{
+        userId: 1,
+        data: [{
+          day: 1,
+          lat: 39.915,
+          lng: 116.404,
+        },{
+          day: 1,
+          lat: 39.917,
+          lng: 116.404,
+        }],
+        label: '用户1'
+      },{
+        userId: 2,
+        data: [{
+          day: 2,
+          lat: 39.915,
+          lng: 116.404,
+        },{
+          day: 2,
+          lat: 39.917,
+          lng: 116.404,
+        }],
+        label: '用户2'
+      }]
+    }
+  },
+  methods: {
+    print: function () {
+      console.log(this.selectedUser)
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0
+} */
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0
+}
+.header {
+  border-bottom: 1px solid #a0a0a0;
+  color: #333;
+  text-align: center;
+  line-height: 3.5rem;
+  /* height: 9vh; */
+}
+.aside {
+  width: 20vw;
+  height: 90.5vh;
+  float: left;
+}
+.main {
+  width: 80vw;
+  height: 90.5vh;
+  float: left;
+}
+.map {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin:0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BaiduMap from 'vue-baidu-map'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+Vue.use(BaiduMap, {
+  ak: 'EaRu4rpgUguMflr8UTqGxuISMOdAV0Lc'
+})
 new Vue({
   render: h => h(App),
 }).$mount('#app')
