8fd2e7b1af1bf0750ddbcac5028fed8a722f5058
lsq210 <1135595767@qq.com>
2019-09-24 17:28:40 +0800

    Delete some annotation

--- src/api/mock/user.mock.js
+++ src/api/mock/user.mock.js
@@ -1,17 +1,6 @@
 import Mock from 'mockjs'
 // import userInfor from './data'
 
-// function getQueryVariable(url, variable){
-//   var query = url.split('?')[1]//截取url中？后的字符串
-//   var vars = query.split("&");
-//   for (var i=0;i<vars.length;i++) {
-//     var pair = vars[i].split("=");
-//     if(pair[0] == variable){
-//       return pair[1];
-//     }
-//   }
-//   return(false);
-// }
 function getUrlQuery (url) {
   var query = url.split('?')[1]
   var pairObject = {}
@@ -36,7 +25,6 @@ export default {
   // Post demo
   postUserInfor: config => {
     // console.log('Mock: ' + config.url)
-    // var selectedUsers = JSON.parse(config.body).param1
     return {
       url: config.url,
       time: Mock.mock('@datetime'),
