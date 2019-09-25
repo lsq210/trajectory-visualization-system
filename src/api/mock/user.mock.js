6492a1aaadbe1783b13baf69063c71602b5009b1
Qingao Chai <chaiqingao@outlook.com>
2019-09-25 17:38:47 +0800

    final

--- src/api/mock/user.mock.js
+++ src/api/mock/user.mock.js
@@ -17,8 +17,33 @@ export default {
     /* eslint-disable */
     console.log(userInfor)
     var variable = getUrlQuery(config.url)
-    console.log(variable)
+    console.log("variable",variable)
+    var res=[]
+    var userIds = variable.userIds.split(",")
+    const startTime = new Date(variable.selectedStart)
+    const endTime = new Date(variable.selectedEnd)
+    startTime.setSeconds(0)
+    endTime.setSeconds(0)
+    for (let i = 0; i < userIds.length; i++) {
+      const userId = userIds[i]
+      var user = userInfor[userId][1]
+      var time1 = new Date(user[0].time)
+      var time2 = new Date(user[user.length-1].time)
+      if (time1 > endTime||time2 < startTime) {
+        continue
+      }
+      var realStartTime = startTime > time1 ? startTime : time1
+      var realEndTime = endTime < time2 ? endTime : time2
+      var index1 = (realStartTime.getTime()-time1.getTime())/1000/60
+      var index2 = (realEndTime.getTime()-time1.getTime())/1000/60
+      var data = user.slice(index1,index2+1)
+      var ele = [userInfor[userId][0]]
+      ele.push(data)
+      res.push(ele)
+    }
+    console.log("res",res)
     return {
+      data: res,
       url: config.url,
       time: Mock.mock('@datetime'),
     }
